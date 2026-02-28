9619e7c42d995743d51c602395a80e271f417c11
zenin5479 <zenin5479@gmail.com>
2026-02-28 12:06:35 +0300

    Update 0.9 Обновлен метод. Проведен рефакторинг

--- FormOne.Designer.cs
+++ FormOne.Designer.cs
@@ -28,19 +28,82 @@
       /// </summary>
       private void InitializeComponent()
       {
+         ButtonClear = new System.Windows.Forms.Button();
+         RichTextBoxReader = new System.Windows.Forms.RichTextBox();
+         ListBoxReader = new System.Windows.Forms.ListBox();
+         TextBoxReader = new System.Windows.Forms.TextBox();
+         ButtonStart = new System.Windows.Forms.Button();
          SuspendLayout();
+         // 
+         // ButtonClear
+         // 
+         ButtonClear.Location = new System.Drawing.Point(668, 12);
+         ButtonClear.Name = "ButtonClear";
+         ButtonClear.Size = new System.Drawing.Size(90, 23);
+         ButtonClear.TabIndex = 18;
+         ButtonClear.Text = "Clear";
+         ButtonClear.UseVisualStyleBackColor = true;
+         // 
+         // RichTextBoxReader
+         // 
+         RichTextBoxReader.Location = new System.Drawing.Point(12, 227);
+         RichTextBoxReader.Name = "RichTextBoxReader";
+         RichTextBoxReader.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+         RichTextBoxReader.Size = new System.Drawing.Size(370, 180);
+         RichTextBoxReader.TabIndex = 17;
+         RichTextBoxReader.Text = "";
+         // 
+         // ListBoxReader
+         // 
+         ListBoxReader.FormattingEnabled = true;
+         ListBoxReader.ItemHeight = 15;
+         ListBoxReader.Location = new System.Drawing.Point(388, 41);
+         ListBoxReader.Name = "ListBoxReader";
+         ListBoxReader.ScrollAlwaysVisible = true;
+         ListBoxReader.Size = new System.Drawing.Size(370, 184);
+         ListBoxReader.TabIndex = 16;
+         // 
+         // TextBoxReader
+         // 
+         TextBoxReader.Location = new System.Drawing.Point(12, 41);
+         TextBoxReader.Multiline = true;
+         TextBoxReader.Name = "TextBoxReader";
+         TextBoxReader.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+         TextBoxReader.Size = new System.Drawing.Size(370, 180);
+         TextBoxReader.TabIndex = 15;
+         // 
+         // ButtonStart
+         // 
+         ButtonStart.Location = new System.Drawing.Point(12, 12);
+         ButtonStart.Name = "ButtonStart";
+         ButtonStart.Size = new System.Drawing.Size(110, 23);
+         ButtonStart.TabIndex = 14;
+         ButtonStart.Text = "Запуск сервера";
+         ButtonStart.UseVisualStyleBackColor = true;
          // 
          // FormOne
          // 
          AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          ClientSize = new System.Drawing.Size(898, 566);
+         Controls.Add(ButtonClear);
+         Controls.Add(RichTextBoxReader);
+         Controls.Add(ListBoxReader);
+         Controls.Add(TextBoxReader);
+         Controls.Add(ButtonStart);
          Name = "FormOne";
          StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
          Text = "Синхронный Json клиент Windows Forms";
          ResumeLayout(false);
+         PerformLayout();
       }
 
       #endregion
+
+      private System.Windows.Forms.Button ButtonClear;
+      private System.Windows.Forms.RichTextBox RichTextBoxReader;
+      private System.Windows.Forms.ListBox ListBoxReader;
+      private System.Windows.Forms.TextBox TextBoxReader;
+      private System.Windows.Forms.Button ButtonStart;
    }
 }
