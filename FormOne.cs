4c06e4293d09d22e08510f8d0f975147b5ab5a4d
zenin5479 <zenin5479@gmail.com>
2026-03-01 11:12:03 +0300

    Update 4.9 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -203,8 +203,8 @@ namespace SyncClientWinForms
             string response = Client.UploadString(url, "PUT", json);
             Item updatedItem = JsonConvert.DeserializeObject<Item>(response);
             //Console.WriteLine("Статус: Обновлено успешно");
-            Console.WriteLine("Date: {0:dd.MM.yyyy HH:mm:ss.fff}, Timestamp: {1}, ID: {2}, Производитель: {3}, Название: {4}, Цена: {5:F}",
-               item.Date, item.Timestamp, item.Id, item.Vendor, item.Name, item.Price);
+            //Console.WriteLine("Date: {0:dd.MM.yyyy HH:mm:ss.fff}, Timestamp: {1}, ID: {2}, Производитель: {3}, Название: {4}, Цена: {5:F}",
+            //   item.Date, item.Timestamp, item.Id, item.Vendor, item.Name, item.Price);
             return updatedItem;
          }
          catch (WebException ex)
