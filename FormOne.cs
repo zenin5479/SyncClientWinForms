06a7e23fbce3fd0d0eca18486dc514e05dfbda8d
zenin5479 <zenin5479@gmail.com>
2026-03-01 11:13:30 +0300

    Update 5.4 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -274,7 +274,7 @@ namespace SyncClientWinForms
             {
                if (response.StatusCode == HttpStatusCode.NotFound)
                {
-                  Console.WriteLine("Статус: Ожидаемая ошибка - элемент не найден");
+                  //Console.WriteLine("Статус: Ожидаемая ошибка - элемент не найден");
                }
                else
                {
