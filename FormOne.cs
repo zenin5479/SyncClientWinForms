d03fa132765721b1e629398613d3e068c4ef059b
zenin5479 <zenin5479@gmail.com>
2026-03-03 09:51:03 +0300

    Update 13.9 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -120,7 +120,7 @@ namespace SyncClientWinForms
             DeleteItem(item3.Id);
 
             // 11. Проверка удаления элемента
-            //Console.WriteLine("\n11. Проверка удаления элемента:");
+           
             TextBoxReader.AppendText("\n11. Проверка удаления элемента:");
             TextBoxReader.AppendText(Environment.NewLine);
             ListBoxReader.Items.Add("\n11. Проверка удаления элемента:");
