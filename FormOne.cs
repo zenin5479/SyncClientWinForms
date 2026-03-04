47214156468369f16db222b8c0b4ae52f2a90f7c
zenin5479 <zenin5479@gmail.com>
2026-03-04 09:29:44 +0300

    Update 18.9 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -223,7 +223,7 @@ namespace SyncClientWinForms
             //Console.WriteLine("1. Сервер недоступен");
             TextBoxReader.AppendText("1. Сервер недоступен");
             TextBoxReader.AppendText(Environment.NewLine);
-            ListBoxReader.Items.Add("1. Сервер доступен");
+            ListBoxReader.Items.Add("1. Сервер недоступен");
             RichTextBoxReader.AppendText("1. Сервер доступен");
             RichTextBoxReader.AppendText(Environment.NewLine);
             throw;
