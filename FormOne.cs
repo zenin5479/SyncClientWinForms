d73640ffcac9414a875de2eb14e29053855b091f
zenin5479 <zenin5479@gmail.com>
2026-03-04 10:51:19 +0300

    Update 21.2 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -284,9 +284,6 @@ namespace SyncClientWinForms
             ListBoxReader.Items.Add("Статус: Создано успешно");
             RichTextBoxReader.AppendText("Статус: Создано успешно");
             RichTextBoxReader.AppendText(Environment.NewLine);
-
-            //Console.WriteLine("Date: {0:dd.MM.yyyy HH:mm:ss.fff}, Timestamp: {1}, ID: {2}, Производитель: {3}, Название: {4}, Цена: {5:F}",
-            //   item.Date, item.Timestamp, item.Id, item.Vendor, item.Name, item.Price);
             
             string wording = ("Date: {0:dd.MM.yyyy HH:mm:ss.fff}, Timestamp: {1}, ID: {2}, Производитель: {3}, Название: {4}, Цена: {5:F}",
                item.Date, item.Timestamp, item.Id, item.Vendor, item.Name, item.Price).ToString();
