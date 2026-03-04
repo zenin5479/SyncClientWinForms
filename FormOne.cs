9b48670131c0c740a2d501c3138953f83a1285bf
zenin5479 <zenin5479@gmail.com>
2026-03-04 11:38:05 +0300

    Update 22.8 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -325,7 +325,6 @@ namespace SyncClientWinForms
             ListBoxReader.Items.Add(wording);
             RichTextBoxReader.AppendText(wording);
             RichTextBoxReader.AppendText(Environment.NewLine);
-
          }
          catch (WebException ex)
          {
