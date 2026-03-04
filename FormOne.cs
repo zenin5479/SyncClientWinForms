38b8bb804c969fdd44802f65d3762aaaaf459a8d
zenin5479 <zenin5479@gmail.com>
2026-03-04 11:36:12 +0300

    Update 22.7 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -348,9 +348,6 @@ namespace SyncClientWinForms
             RichTextBoxReader.AppendText("Статус: Обновлено успешно");
             RichTextBoxReader.AppendText(Environment.NewLine);
 
-            //Console.WriteLine("Date: {0:dd.MM.yyyy HH:mm:ss.fff}, Timestamp: {1}, ID: {2}, Производитель: {3}, Название: {4}, Цена: {5:F}",
-            //   item.Date, item.Timestamp, item.Id, item.Vendor, item.Name, item.Price);
-
             string wording = ("Date: {0:dd.MM.yyyy HH:mm:ss.fff}, Timestamp: {1}, ID: {2}, Производитель: {3}, Название: {4}, Цена: {5:F}",
                item.Date, item.Timestamp, item.Id, item.Vendor, item.Name, item.Price).ToString();
 
