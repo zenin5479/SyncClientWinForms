e91f663aa9e424ff44049cca202359db59f34385
zenin5479 <zenin5479@gmail.com>
2026-03-04 11:21:29 +0300

    Update 21.3 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -311,6 +311,8 @@ namespace SyncClientWinForms
             string response = Client.DownloadString(url);
             Item item = JsonConvert.DeserializeObject<Item>(response);
             //Console.WriteLine("Статус: Найден");
+
+
             //Console.WriteLine("Date: {0:dd.MM.yyyy HH:mm:ss.fff}, Timestamp: {1}, ID: {2}, Производитель: {3}, Название: {4}, Цена: {5:F}",
             //   item.Date, item.Timestamp, item.Id, item.Vendor, item.Name, item.Price);
          }
