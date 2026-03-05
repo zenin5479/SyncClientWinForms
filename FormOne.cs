c26c37c98e0672e8a532b936a95a7198b6728a2c
zenin5479 <zenin5479@gmail.com>
2026-03-05 18:54:25 +0300

    Update 26.1 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -451,7 +451,7 @@ namespace SyncClientWinForms
 
                   TextBoxReader.AppendText("Статус: Ожидаемая ошибка - элемент не найден");
                   TextBoxReader.AppendText(Environment.NewLine);
-                  ListBoxReader.Items.Add("Статус: ОШИБКА - элемент удален (не должно было произойти)");
+                  ListBoxReader.Items.Add("Статус: Ожидаемая ошибка - элемент не найден");
                   RichTextBoxReader.AppendText("Статус: ОШИБКА - элемент удален (не должно было произойти)");
                   RichTextBoxReader.AppendText(Environment.NewLine);
                }
