b343ff778c167ea42174b72a1867c42198a35983
zenin5479 <zenin5479@gmail.com>
2026-03-05 18:55:07 +0300

    Update 26.3 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -448,7 +448,6 @@ namespace SyncClientWinForms
                if (response.StatusCode == HttpStatusCode.NotFound)
                {
                   //Console.WriteLine("Статус: Ожидаемая ошибка - элемент не найден");
-
                   TextBoxReader.AppendText("Статус: Ожидаемая ошибка - элемент не найден");
                   TextBoxReader.AppendText(Environment.NewLine);
                   ListBoxReader.Items.Add("Статус: Ожидаемая ошибка - элемент не найден");
