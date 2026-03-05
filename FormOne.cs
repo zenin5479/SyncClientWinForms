b07a4c9a63aeb8e181b4387d795790f753671386
zenin5479 <zenin5479@gmail.com>
2026-03-05 18:44:21 +0300

    Update 24.2 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -402,7 +402,7 @@ namespace SyncClientWinForms
             TextBoxReader.AppendText("Статус: ОШИБКА - элемент найден (не должно было произойти)");
             TextBoxReader.AppendText(Environment.NewLine);
             ListBoxReader.Items.Add("Статус: ОШИБКА - элемент найден (не должно было произойти)");
-            RichTextBoxReader.AppendText("Статус: Удалено успешно");
+            RichTextBoxReader.AppendText("Статус: ОШИБКА - элемент найден (не должно было произойти)");
             RichTextBoxReader.AppendText(Environment.NewLine);
          }
          catch (WebException ex)
