0e49f0643ee6563b155d8f195791b1909757843e
zenin5479 <zenin5479@gmail.com>
2026-03-07 11:49:53 +0300

    Update 30.4 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -537,7 +537,7 @@ namespace SyncClientWinForms
             {
                if (response.StatusCode == HttpStatusCode.MethodNotAllowed)
                {
-                  //Console.WriteLine("Статус: Ожидаемая ошибка - метод не разрешен");
+                  
                   TextBoxReader.AppendText("Статус: Ожидаемая ошибка - метод не разрешен");
                   TextBoxReader.AppendText(Environment.NewLine);
                   ListBoxReader.Items.Add("Статус: Ожидаемая ошибка - метод не разрешен");
