95e3158ad39acfda0a0e9639980c543f4fd37b5a
zenin5479 <zenin5479@gmail.com>
2026-03-08 16:48:15 +0300

    Update 32.5 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -589,7 +589,7 @@ namespace SyncClientWinForms
          {
             //Console.WriteLine("Ошибка: {0}", ex.Message);
 
-            TextBoxReader.AppendText("Тело ошибки: " + errorBody);
+            TextBoxReader.AppendText("Ошибка: {0}", ex.Message);
             TextBoxReader.AppendText(Environment.NewLine);
             ListBoxReader.Items.Add("Тело ошибки: " + errorBody);
             RichTextBoxReader.AppendText("Тело ошибки: " + errorBody);
