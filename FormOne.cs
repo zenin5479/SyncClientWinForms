d79aa46ae68cbe2a201ec7849c31e836763c8cc6
zenin5479 <zenin5479@gmail.com>
2026-03-09 10:34:57 +0300

    Update 34.7 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -562,7 +562,7 @@ namespace SyncClientWinForms
             TextBoxReader.AppendText("HTTP Ошибка: " + (int)response.StatusCode + " " + response.StatusCode);
             TextBoxReader.AppendText(Environment.NewLine);
             ListBoxReader.Items.Add("HTTP Ошибка: " + (int)response.StatusCode + " " + response.StatusCode);
-            RichTextBoxReader.AppendText("HTTP Ошибка: " + (int)response.StatusCode + response.StatusCode);
+            RichTextBoxReader.AppendText("HTTP Ошибка: " + (int)response.StatusCode + " " + response.StatusCode);
             RichTextBoxReader.AppendText(Environment.NewLine);
 
             using (Stream stream = response.GetResponseStream())
