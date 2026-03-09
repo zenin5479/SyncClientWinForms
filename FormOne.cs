82003590a8a729990fd85f57d84a7a633a13d342
zenin5479 <zenin5479@gmail.com>
2026-03-09 10:17:14 +0300

    Update 33.7 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -256,7 +256,7 @@ namespace SyncClientWinForms
                   string wording = string.Format("Date: {0:dd.MM.yyyy HH:mm:ss.fff}, Timestamp: {1}, ID: {2}, Производитель: {3}, Название: {4}, Цена: {5:F}",
                      item.Date, item.Timestamp, item.Id, item.Vendor, item.Name, item.Price);
                   //
-                  TextBoxReader.AppendText();
+                  TextBoxReader.AppendText(wording);
                   //
                   TextBoxReader.AppendText(Environment.NewLine);
                   ListBoxReader.Items.Add(wording);
